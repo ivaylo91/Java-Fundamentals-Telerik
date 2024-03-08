9a0371172ed5b22a46a31bf0473c0ed64a2ddc2b
ivaylo91 <ipenev91@gmail.com>
2024-03-08 12:26:04 +0200

    Find Longest String

---
+++ src/com/java/loops/LongestString.java
@@ -0,0 +1,26 @@
+package com.java.loops;
+
+import java.util.Scanner;
+
+public class LongestString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int countLength = 0;
+        String biggestSize = "";
+
+        while (!input.equals("END")) {
+            String lunch = input;
+            int length = lunch.length();
+
+            if (length >= countLength) {
+                countLength = length;
+                biggestSize = input;
+            }
+
+            input = scanner.nextLine();
+        }
+        System.out.println(biggestSize);
+
+    }
+}
